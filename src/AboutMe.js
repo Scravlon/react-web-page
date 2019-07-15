710c05c06e64b36d95dc5a5a644c5fe8a0d3f64b
Scravlon <haokokyong10@gmail.com>
2019-07-14 23:19:14 -0400

    Warming fixed

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,27 +10,29 @@ function About() {
           About Me
         </h1>
         <hr width="5%" color="black"/>
-        <img src={mepic} width="225px" height="300px" />
+        <img src={mepic} alt="me" width="225px" height="300px" />
         <h2>
           Education
         </h2>
-        <table class="tg">
+        <table className="tg">
+        <tbody>
           <tr>
-            <th class="tg-0pky"><strong>University at Buffalo, SUNY (Transfer)</strong></th>
-            <th class="tg-dvpl"><strong>Expected Graduate: Winter 2019</strong></th>
+            <th className="tg-0pky"><strong>University at Buffalo, SUNY (Transfer)</strong></th>
+            <th className="tg-dvpl"><strong>Expected Graduate: Winter 2019</strong></th>
           </tr>
           <tr>
-            <td class="tg-0pky">Bachelor of Science, Computer Science</td>
-            <td class="tg-dvpl">Dean's List | GPA: 3.8/4.0</td>
+            <td className="tg-0pky">Bachelor of Science, Computer Science</td>
+            <td className="tg-dvpl">Dean's List | GPA: 3.8/4.0</td>
           </tr>
           <tr>
-            <td class="tg-0pky"><strong>Inti International College Subang Malaysia</strong></td>
-            <td class="tg-dvpl"><strong>Spring 2016 - Summer 2017</strong></td>
+            <td className="tg-0pky"><strong>Inti International College Subang Malaysia</strong></td>
+            <td className="tg-dvpl"><strong>Spring 2016 - Summer 2017</strong></td>
           </tr>
           <tr>
-            <td class="tg-0pky">Bachelor of Science, Computer Science</td>
-            <td class="tg-dvpl">Dean's List | GPA: 3.6/4.0</td>
+            <td className="tg-0pky">Bachelor of Science, Computer Science</td>
+            <td className="tg-dvpl">Dean's List | GPA: 3.6/4.0</td>
           </tr>
+          </tbody>
         </table>
       </div>
     </div>

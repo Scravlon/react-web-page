461b1626c9835a8577c0402b4d9e7f46dc4640e6
Scravlon <haokokyong10@gmail.com>
2020-02-12 20:57:15 -0500

    buttom usable

--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,7 @@ function HOME() {
         <p>Software Developer</p>
         <p>B.S. Computer Science - University at Buffalo (SUNY)</p>
       </header>
-      <dir className="App-down">
+      <dir className="App-down" style="text-decoration: none;color:white">
         <dir id="arrow-down"><FaChevronCircleDown/></dir>
 
         </dir>
